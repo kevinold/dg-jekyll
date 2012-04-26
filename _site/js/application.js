bd867f09133bb399bd2114314f10528527b43201
Kevin Old <kevin@kevinold.com>
2012-04-26 10:55:50 -0500

    updates to default template; add contact page; add top nav; add GA code; add footer

---
+++ _site/js/application.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.dropdown-toggle').dropdown();
+
+  /*$('#myForm').submit(function() {
+    $.post({
+      url: 'https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8',
+      dataType: 'json',
+      data: $('#myForm').serialize(),
+    });
+  });*/
+		
+});
+
