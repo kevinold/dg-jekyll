534a2c7d3c8c5b77471ea080502c2f9b4ee17b4e
Kevin Old <kevin@kevinold.com>
2012-05-03 12:43:20 -0500

    dynamically add referring url to tell me more link

--- _site/js/application.js
+++ _site/js/application.js
@@ -1,3 +1,25 @@
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash; 
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); // cut the URL string down to just everything after the ?, split this into an array with information like this: array [0] = "var=xxx"; array [1] = "var2=yyy";
+
+   //loop through each item in that array
+    for(var i = 0; i < hashes.length; i++)
+    {
+        //split the item at the "="
+        hash = hashes[i].split('=');
+
+        //put the value name of the first variable into the "vars" array
+        vars.push(hash[0]);
+
+        //assign the value to the variable name, now you can access it like this:
+        // alert(vars["var1"]); //alerts the value of the var1 variable from the url string
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
 $(document).ready(function() {
 
   $('.dropdown-toggle').dropdown();
@@ -5,5 +27,11 @@ $(document).ready(function() {
     interval: 7000
   });
 
+  // add page to tellMeMore url
+  if ($('#tellMeMore')) {
+    $('#tellMeMore').attr('href', $('#tellMeMore').attr('href') + '?page=' + document.URL.split('/').pop());
+  }
+
+  $('#tellMeMoreForm [name=website]').val(getUrlVars().page);
 });
 
