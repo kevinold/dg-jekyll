56fb1d6b65d1c99b28d523ef4275b53ea2a31b34
Kevin Old <kevin@kevinold.com>
2012-06-13 12:24:55 -0500

    add videos to solutions pages

--- _site/js/application.js
+++ _site/js/application.js
@@ -22,14 +22,26 @@ function getUrlVars()
 
 $(document).ready(function() {
 
+  // determine videos for home and solutions pages displayed in a modal
   var videos = ['0bymXYUYmwE','QptCT672CAc','KHXuQt6HMbA'];
-  var rand = Math.floor(Math.random()*3);
-  var featured_video_img = 'homeVideo' + rand + '.png';
-  $('#video_image').attr("src","/img/" + featured_video_img);
+  var video_code;
 
-  var video_iframe = _.template('<iframe width="560" height="315" src="http://www.youtube.com/embed/<%= video_code %>?rel=0&wmode=transparent" frameborder="0" allowfullscreen></iframe>', { video_code: videos[rand] });
+  // check if data-video-num (only defined on solutions pages)
+  if ($('#video_image').attr("data-video-num")) {
+    video_code = videos[$('#video_image').attr("data-video-num")];
+  } else {
+    // randomly load homepage video and image
+    var rand = Math.floor(Math.random()*3);
+    var featured_video_img = 'homeVideo' + rand + '.png';
+    video_code = videos[rand];
+    $('#video_image').attr("src","/img/" + featured_video_img);
+  }
+
+  var video_iframe = _.template('<iframe width="560" height="315" src="http://www.youtube.com/embed/<%= video_code %>?rel=0&wmode=transparent" frameborder="0" allowfullscreen></iframe>', { video_code: video_code });
   $('#VideoModal .modal-body').html(video_iframe);
   
+
+  // initialize carousel on homepage
   $('.dropdown-toggle').dropdown();
   $('.carousel').carousel({
     interval: 7000
@@ -50,6 +62,7 @@ $(document).ready(function() {
     $('#tellMeMore').attr('href', $('#tellMeMore').attr('href') + '?page=' + document.URL.split('/').pop());
   }
 
+  // page tracking for tell me more form
   $('#tellMeMoreForm [name=website]').val(getUrlVars().page);
 
   // Add active class to left nav
